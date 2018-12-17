0dc0397c6fadb0356985b1f0c5490a0039ae4260
MadeYoga <vngla21@gmail.com>
2018-12-17 19:17:16 +0700

    update World Cup Prediction

--- Predict World Cup/predict.py
+++ Predict World Cup/predict.py
@@ -54,9 +54,9 @@ print("CrossValidation Accuracy: %.2f%%" % (
 print("=====================================\nMulti-Layer Perceptron:")
 mlp = MLPClassifier(
     activation='tanh',
-    hidden_layer_sizes=(6,10),
+    hidden_layer_sizes=(5,2),
     alpha=1e-3,
-    max_iter=3000
+    max_iter=1000
     )
 
 mlp.fit(X_train, y_train)
@@ -99,3 +99,61 @@ print("CrossValidation Accuracy: %.2f%%" % (
     cross_val_score(knn, X_train, y_train, cv=3).mean() * 100
     ))
 print("=====================================")
+
+ranking = pd.read_csv('Datasets/fifa_rankings.csv')
+fixtures = pd.read_csv('Datasets/fixtures.csv')
+
+pred_set = []
+fixtures.insert(
+    1, 'first_position',
+    fixtures['Home Team'].map(ranking.set_index('Team')['Position'])
+    )
+fixtures.insert(
+    2, 'second_position',
+    fixtures['Away Team'].map(ranking.set_index('Team')['Position'])
+    )
+fixtures = fixtures.iloc[:48, :]
+print(fixtures.head())
+
+for index, row in fixtures.iterrows():
+    if row['first_position'] < row['second_position']:
+        pred_set.append({'home_team': row['Home Team'], 'away_team': row['Away Team'], 'winning_team': None})
+    else:
+        pred_set.append({'home_team': row['Away Team'], 'away_team': row['Home Team'], 'winning_team': None})
+        
+pred_set = pd.DataFrame(pred_set)
+backup_pred_set = pred_set
+
+print(pred_set.head())
+
+
+# Get dummy variables and drop winning_team column
+pred_set = pd.get_dummies(pred_set, prefix=['home_team', 'away_team'], columns=['home_team', 'away_team'])
+
+# Add missing columns compared to the model's training dataset
+missing_cols = set(final.columns) - set(pred_set.columns)
+for c in missing_cols:
+    pred_set[c] = 0
+pred_set = pred_set[final.columns]
+
+# Remove winning team column
+pred_set = pred_set.drop(['winning_team'], axis=1)
+
+print(pred_set.head())
+
+#group matches 
+predictions = log_reg.predict(pred_set)
+for i in range(fixtures.shape[0]):
+    print(backup_pred_set.iloc[i, 1] + " and " + backup_pred_set.iloc[i, 0])
+    if predictions[i] == 2:
+        print("Winner: " + backup_pred_set.iloc[i, 1])
+    elif predictions[i] == 1:
+        print("Draw")
+    elif predictions[i] == 0:
+        print("Winner: " + backup_pred_set.iloc[i, 0])
+    print('Probability of ' + backup_pred_set.iloc[i, 1] + ' winning: ', '%.3f'%(log_reg.predict_proba(pred_set)[i][2]))
+    print('Probability of Draw: ', '%.3f'%(log_reg.predict_proba(pred_set)[i][1]))
+    print('Probability of ' + backup_pred_set.iloc[i, 0] + ' winning: ', '%.3f'%(log_reg.predict_proba(pred_set)[i][0]))
+    print("")
+
+
--- Predict World Cup/preprocessing_data.py
+++ Predict World Cup/preprocessing_data.py
@@ -7,17 +7,34 @@ results = pd.read_csv('Datasets/results.csv')
 print(world_cup.head())
 print(results.head())
 
+## filter year
+year = []
+for row in results['date']:
+    year.append(int(row[:4]))
+results['match_year'] = year
+results = results[results['match_year'] >= 1930]
+print(results.head())
+
+## Categoryin goal difference
+results['goal_difference'] = np.absolute(results['home_score'] - results['away_score'])
+gd_category = []
+for i in range(len(results['goal_difference'])):
+    if results['goal_difference'].iloc[i] <= 0:
+        gd_category.append('low')
+    else:
+        gd_category.append('high')
+results['gd_category'] = gd_category
+print(results.head())
+
 winners = []
 for i in range(len(results['home_team'])):
-    if results['home_score'][i] > results['away_score'][i]:
-        winners.append(results['home_team'][i])
-    elif results['home_score'][i] < results['away_score'][i]:
-        winners.append(results['away_team'][i])
+    if results['home_score'].iloc[i] > results['away_score'].iloc[i] and results['gd_category'].iloc[i] == 'high':
+        winners.append(results['home_team'].iloc[i])
+    elif results['home_score'].iloc[i] < results['away_score'].iloc[i] and results['gd_category'].iloc[i] == 'high':
+        winners.append(results['away_team'].iloc[i])
     else:
         winners.append('Draw')
-
 results['winning_team'] = winners
-results['goal_difference'] = np.absolute(results['home_score'] - results['away_score'])
 
 print(results.head())
 
@@ -39,11 +56,11 @@ df_teams.drop_duplicates()
 print(df_teams.count())
 print(df_teams.tail())
 
-year = []
-for row in df_teams['date']:
-    year.append(int(row[:4]))
-df_teams['match_year'] = year
-df_teams = df_teams[df_teams.match_year >= 1998]
+##year = []
+##for row in df_teams['date']:
+##    year.append(int(row[:4]))
+##df_teams['match_year'] = year
+##df_teams = df_teams[df_teams.match_year >= 1998]
 
 ## drop kolom yang tidak mempengaruhi hasil match
 df_teams = df_teams.drop(
@@ -55,7 +72,8 @@ df_teams = df_teams.drop(
         'city',
         'country',
         'goal_difference',
-        'match_year'
+        'match_year',
+        'gd_category'
     ],
     axis=1
 )
