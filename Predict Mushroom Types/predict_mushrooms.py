ef05b32d6304c3c0ef052a7cd414190a387ca623
MadeYoga <vngla21@gmail.com>
2019-06-07 23:38:22 +0700

    Update

--- Predict Mushroom Types/predict_mushrooms.py
+++ Predict Mushroom Types/predict_mushrooms.py
@@ -23,18 +23,37 @@ from sklearn.preprocessing import LabelEncoder
 X = encode_label(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=4)
 
-from sklearn.neural_network import MLPClassifier
-clf = MLPClassifier(
-    hidden_layer_sizes=(5,2),
-    activation='logistic',
-    max_iter=1000
-    )
+#from sklearn.neural_network import MLPClassifier
+#clf = MLPClassifier(
+#    hidden_layer_sizes=(5,2),
+#    activation='logistic',
+#    max_iter=1000
+#    )
 
+# clf.fit(X_train, y_train.values.ravel())
+
+# predicted_y = clf.predict(X_test)
+
+#from sklearn.metrics import accuracy_score
+#score = accuracy_score(predicted_y, y_test)
+
+#print("Test Accuracy: %.2f%%" % (score * 100))
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals.six import StringIO
+from sklearn.tree import export_graphviz
+from IPython.display import Image
+import pydotplus
+
+clf = RandomForestClassifier(n_estimators=100)
 clf.fit(X_train, y_train.values.ravel())
 
-predicted_y = clf.predict(X_test)
-
-from sklearn.metrics import accuracy_score
-score = accuracy_score(predicted_y, y_test)
-
-print("Test Accuracy: %.2f%%" % (score * 100))
+dot_data = StringIO()
+export_graphviz(clf.estimators_[0],
+                out_file=dot_data,
+                feature_names=X_train.columns,
+                filled=True,
+                rounded=True,
+                special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+Image(graph.create_png())
